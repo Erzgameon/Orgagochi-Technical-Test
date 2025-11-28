a0a6d4811c8c309885ba8d6c562f2629e156e41e
Erzgameon <gleb.kamnev@gmail.com>
2025-11-28 12:31:44 +0100

    I have implemented the note drawing system more ellegantly

--- sketch.js
+++ sketch.js
@@ -2,6 +2,7 @@ let notes;
 
 function setup() {
   createCanvas(400, 500);
+  background(240);
 
   notes = [];
 
@@ -25,20 +26,30 @@ function setup() {
 
   addNote("This is a test note");
   addNote("This is a test note");
-  addNote("This is a test note");
-  
    
+  drawNotes();
 }
 
 function addNote(_text) {
   notes.push(new Note(_text,notes.length));
-  redraw();
 }
 
-function redraw() {
-  background(240);
+function drawNotes() {
+  let x;
+  let y;
+
   for (let i = 0; i < notes.length; i++) {
-    notes[i].display();
+    x = 10;
+    y = 10 + (notes[i].h + 10) * i;
+
+    
+
+    fill(notes[i].color);
+    rect(x, y, notes[i].w, notes[i].h);
+    fill(0);
+    textSize(12);
+    text(notes[i].text, x + 10, y + 10, notes[i].w - 20, notes[i].h - 20);
+
   }
 }
 
@@ -49,18 +60,10 @@ class Note {
 
     this.w = 150;
     this.h = 150;
-    this.x = 10;
-    this.y = 10 + (this.h + 10) * index;
+    //this.x = 10;
+    //this.y = 10 + (this.h + 10) * index;
     
     this.color = color(255, 255, 150);
-    this.display();
-  }
 
-  display() {
-    fill(this.color);
-    rect(this.x, this.y, this.w, this.h);
-    fill(0);
-    textSize(12);
-    text(this.text, this.x + 10, this.y + 20, this.w - 20, this.h - 20);
   }
 }
